5dc8fc63681ce58c72266dcb990f3104c7f3866f
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2020-01-07 17:19:13 +0000

    first commit

---
+++ todo.js
@@ -0,0 +1,72 @@
+//Reference to DOM elements which will be manipulate within the program
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || '';
+
+rendertodos();
+/*
+Fill up our array from local storage.
+In case of the local storage be empty in order to avoid error we put a standard value on array ('')
+Note that the list_todos on local storage was in JSON string format and we need converts it to array using the method parse
+*/
+
+//function to render the modification performed on DOM elements
+function rendertodos(){
+    listElement.innerHTML = ''; // Manipulate the text inside one especific element on HTML
+    for(todo of todos)
+    {
+        if (todo != ' ')
+        {
+            var todoElement = document.createElement('li'); // Create the element li on HTML
+            var todoText = document.createTextNode(todo); // Create a text node to attach on list element
+            
+            var linkElement = document.createElement('a'); // Create a link element 
+            linkElement.setAttribute('href' , '#'); // add a atribute to the link element || the '#'means that the attribute will be added without effect
+            var linkText = document.createTextNode("Delete"); // Create a text node to attacth on link element
+            
+            var pos = todos.indexOf(todo); //Search the index inside the array by the text gaven.
+
+            linkElement.setAttribute('onclick' , 'deleteTodo(' + pos + ')'); 
+            /*
+                This part of code is very important !!!
+                On this line we set the caller to function "deleteTodo" as an attribute of link element.
+            
+            */ 
+            
+            linkElement.appendChild(linkText); // attaching the Childs 
+            todoElement.appendChild(todoText); 
+            todoElement.appendChild(linkElement);
+            listElement.appendChild(todoElement);
+        }
+    }
+}
+
+function addTodos(){
+    var todo = inputElement.value; // Get the value inserted on input box
+    todos.push(todo + ' '); // Add the text on end of the array
+    inputElement.value = ''; // Clear the input box
+    rendertodos(); // Show the new element on screen
+    saveOnLocalStorage(); // Saves each modification on todo_lists
+}
+
+function deleteTodo(pos){
+    todos.splice(pos, 1); // Remove n elements (parameter 2), start on position (parameter 1)
+    rendertodos(); // Show the new todo on screen 
+    saveOnLocalStorage(); // Saves each modification on  todo_lists
+}
+
+buttonElement.onclick = addTodos; // Add the new element whem the button is clicked
+
+function saveOnLocalStorage()
+{
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+    /*
+        This function storages values on local storage with key::vale
+        It only recognizes two strings as valid parameters, so we need to convert the array to a JSON string.
+
+
+    */
+}
